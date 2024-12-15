84be0d482296fd983ad1e0cf70ad8fe167eaa9c4
Ausgore <glenn.marcell.tjandra@gmail.com>
2024-12-15 20:02:29 +0800

    POST /analyse

--- src/lib/maslow.ts
+++ src/lib/maslow.ts
@@ -1,18 +1,26 @@
 import axios from "axios";
-
+import { get } from "svelte/store";
+import { history } from "./stores/history";
 class Maslow {
-  private history: Message[] = [];
   private need: number | null = null;
 
-  async start() {
-    const response = await axios.get<{ need: number, message: Message }>(`${import.meta.env.VITE_API}/start`);
-    this.history.push(response.data.message);
-    this.need = response.data.need;
-    return response.data.message;
+  async send(content: string) {
+    history.update(current => [...current, { role: 'user', content }]);
+    if (get(history).length == 1) {
+      const response = await axios.post<{ need: number, message: Message }>(`${import.meta.env.VITE_API}/start`, { content });
+      this.need = response.data.need;
+      history.update(current => [...current, response.data.message]);
+      return response.data.message;
+    }
+    else {
+      const response = await axios.post<Message>(`${import.meta.env.VITE_API}/reply`, { content, history: get(history), need: this.need });
+      history.update(current => [...current, response.data]);
+      return response.data;
+    }
   }
 
-  async reply(content: string) {
-    const response = await axios.post<Message>(`${import.meta.env.VITE_API}/reply`, { content, history: this.history, need: this.need });
+  async analyse(content: string) {
+    const response = await axios.post<string>(`${import.meta.env.VITE_API}/analyse`, { content, history: get(history), need: this.need });
     return response.data;
   }
 }
